3caab216c892902e5dd64667b1ed74fba5f6dde6
Jjunjiii’ <drum1701@gmail.com>
2024-01-09 23:13:14 +0900

    question 5

--- javascript.js
+++ javascript.js
@@ -134,3 +134,17 @@ const solution4 = (strArr) => {
   }, []);
   return result;
 };
+
+/////////////// 5. 문자열 곱하기  ///////////////
+
+// 문제
+// 문자열 my_string과 정수 k가 주어질 때, my_string을 k번 반복한 문자열을
+// return 하는 solution 함수를 작성해 주세요.
+
+// 생각
+// 1. k 만큼 반복 생성
+// 2. 생성한 문자열 합치기 ( repeat() 메소드 )
+
+const solution = (my_string, k) => {
+  return my_string.repeat(k);
+};
