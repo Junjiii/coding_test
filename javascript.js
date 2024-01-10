44a4873722cbadfb6e94627a4241a30b3edb7e19
Jjunjiii’ <drum1701@gmail.com>
2024-01-10 13:50:30 +0900

    question 7

--- javascript.js
+++ javascript.js
@@ -171,3 +171,12 @@ const solution6 = (str1, str2) => {
   }
   return c.join("");
 };
+
+/////////////// 7. 두 수의 합  ///////////////
+
+// 문제
+// 정수 num1과 num2가 주어질 때, num1과 num2의 합을 return하도록 soltuion 함수를 완성해주세요.
+
+const solution7 = (num1, num2) => {
+  return num1 + num2;
+};
