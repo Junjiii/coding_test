e8d6357eebf6d0e5f6a0406733976d31ec23647e
Jjunjiii’ <drum1701@gmail.com>
2024-01-12 20:56:56 +0900

    question 13

--- javascript.js
+++ javascript.js
@@ -274,3 +274,19 @@ const solution12 = (num_list, n) => {
   let arr = num_list.splice(n, num_list.length);
   return arr.concat(num_list);
 };
+
+/////////////// 13. 접미사인지 확인하기   ///////////////
+
+// 문제
+// 어떤 문자열에 대해서 접미사는 특정 인덱스부터 시작하는 문자열을 의미합니다.
+// 예를 들어, "banana"의 모든 접미사는 "banana", "anana", "nana", "ana", "na", "a"입니다.
+// 문자열 my_string과 is_suffix가 주어질 때,
+// is_suffix가 my_string의 접미사라면 1을, 아니면 0을 return 하는 solution 함수를 작성해 주세요.
+
+// 생각
+// 1. 접미사 : 단어 끝문자를 시작으로 이어지는 단어
+// 2. my_string 끝부분부터 비교 (endWith() 메소드)
+
+const solution13 = (string, include) => {
+  return Number(string.endsWith(include));
+};
