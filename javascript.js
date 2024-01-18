56d91627a9115b55a7c9a06daa411cd9b01c3698
Jjunjiii’ <drum1701@gmail.com>
2024-01-18 10:32:39 +0900

    question 27

--- javascript.js
+++ javascript.js
@@ -582,3 +582,13 @@ const solution26 = (str_list) => {
     return str_list.slice(index + 1);
   }
 };
+
+/////////////// 27. flag에 따라 다른 값 반환하기  ///////////////
+
+// 문제
+// 두 정수 a, b와 boolean 변수 flag가 매개변수로 주어질 때,
+// flag가 true면 a + b를 false면 a - b를 return 하는 solution 함수를 작성해 주세요.
+
+const solution27 = (a, b, flag) => {
+  return flag === true ? a + b : a - b;
+};
