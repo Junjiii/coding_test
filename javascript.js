85474d8ea25d97362c536a348886fa87fd49a035
Jjunjiii’ <drum1701@gmail.com>
2024-01-21 19:59:42 +0900

    question 32

--- javascript.js
+++ javascript.js
@@ -670,3 +670,21 @@ const solution30 = (todo_list, finished) => {
 const solution31 = (myString, pat) => {
   return +myString.toLowerCase().includes(pat.toLowerCase());
 };
+
+/////////////// 32. n보다 커질때까지 더하기  ///////////////
+
+// 문제
+// 정수 배열 numbers와 정수 n이 매개변수로 주어집니다. numbers의 원소를 앞에서부터 하나씩 더하다가
+// 그 합이 n보다 커지는 순간 이때까지 더했던 원소들의 합을 return 하는 solution 함수를 작성해 주세요.
+
+// 생각
+// 1. 반복적으로 수행한다.
+// 2. 조건에 맞을때 멈춘다.
+
+const solution32 = (numbers, n) => {
+  let result = 0;
+
+  for (let i = 0; i <= numbers.length && (result += numbers[i]) <= n; i++);
+
+  return result;
+};
