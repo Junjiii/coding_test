cebdc686e5db3fbb587df5764aa6ef8334b333b8
Jjunjiii’ <drum1701@gmail.com>
2024-01-21 20:15:26 +0900

    question 33

--- javascript.js
+++ javascript.js
@@ -699,3 +699,28 @@ function solution32(numbers, n) {
   }
   return answer;
 }
+
+/////////////// 33. 배열에서 문자열 대소문자 변환하기 ///////////////
+
+// 문제
+// 문자열 배열 strArr가 주어집니다. 모든 원소가 알파벳으로만 이루어져 있을 때,
+// 배열에서 홀수번째 인덱스의 문자열은 모든 문자를 대문자로,
+// 짝수번째 인덱스의 문자열은 모든 문자를 소문자로 바꿔서 반환하는 solution 함수를 완성해 주세요.
+
+// 생각
+// 1. 반복한다.
+// 2. 짝수 혹수 일떄 값을 변환하여 새로운 배열에 추가한다.
+
+function solution33(strArr) {
+  let resultArr = [];
+
+  for (let i = 0; i < strArr.length; i++) {
+    if (i % 2 === 0) {
+      resultArr.push(strArr[i].toLowerCase());
+    } else {
+      resultArr.push(strArr[i].toUpperCase());
+    }
+  }
+
+  return resultArr;
+}
