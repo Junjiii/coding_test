d247f6ceee1d70bf718816ef4578478ead44047e
Jjunjiii’ <drum1701@gmail.com>
2024-01-22 13:31:20 +0900

    question 35

--- javascript.js
+++ javascript.js
@@ -758,3 +758,16 @@ const solution34_2 = (myString) => {
     .map((str) => (["a", "A"].includes(str) ? "A" : str.toLowerCase()))
     .join("");
 };
+
+/////////////// 35. 특정한 문자를 대문자로 바꾸기 ///////////////
+
+// 문제
+// 영소문자로 이루어진 문자열 my_string과 영소문자 1글자로 이루어진 문자열 alp가 매개변수로 주어질 때,
+// my_string에서 alp에 해당하는 모든 글자를 대문자로 바꾼 문자열을 return 하는 solution 함수를 작성해 주세요.
+
+// 생각
+// 1. 조건에 맞는 alp 를 대문자로 대체한다. (replaceAll())
+
+const solution35 = (my_string, alp) => {
+  return my_string.replaceAll(alp, alp.toUpperCase());
+};
