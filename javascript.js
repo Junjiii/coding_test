d6339904d878788ce00b44c15168cc28a26d5f58
Jjunjiii’ <drum1701@gmail.com>
2024-01-25 14:31:12 +0900

    question 41

--- javascript.js
+++ javascript.js
@@ -871,3 +871,17 @@ const solution40 = (my_string) => {
 const solution40_1 = (my_string) => {
   return my_string.trim().split(/ +/);
 };
+
+/////////////// 41. x 사이의 개수 //////////////
+
+// 문제
+// 문자열 myString이 주어집니다. myString을 문자 "x"를 기준으로 나눴을 때
+// 나눠진 문자열 각각의 길이를 순서대로 저장한 배열을 return 하는 solution 함수를 완성해 주세요.
+
+// 생각
+// 1. "x" 를 기준으로 쪼갠다 (split())
+// 2. 배열은 순회하며 나눠진 값으 길이를 구한다. (map())
+
+const solution41 = (myString) => {
+  return myString.split("x").map((v) => v.length);
+};
