76f9fc0c13c8f7d5670711fdd2208b1cff20c718
Jjunjiii’ <drum1701@gmail.com>
2024-01-25 14:44:54 +0900

    question 42

--- javascript.js
+++ javascript.js
@@ -885,3 +885,19 @@ const solution40_1 = (my_string) => {
 const solution41 = (myString) => {
   return myString.split("x").map((v) => v.length);
 };
+
+/////////////// 42. 문자열 잘라서 정렬하기 //////////////
+
+// 문제
+// 문자열 myString이 주어집니다.
+//  "x"를 기준으로 해당 문자열을 잘라내 배열을 만든 후 사전순으로 정렬한 배열을 return 하는 solution 함수를 완성해 주세요.
+// 단, 빈 문자열은 반환할 배열에 넣지 않습니다.
+
+// 생각
+// 1. "x" 를 기준으로 잘라낸다. (split())
+// 2. 값이 있는것만 골라낸다.(filter(Boolean))
+// 3. 오름차순으로 정렬한다. (sort())
+
+const solution42 = (myString) => {
+  return myString.split("x").filter(Boolean).sort();
+};
