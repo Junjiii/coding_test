283a470b941291a0e8a9763f4ecdbcbd1c506755
Jjunjiii’ <drum1701@gmail.com>
2024-01-30 21:36:05 +0900

    question 49

--- javascript.js
+++ javascript.js
@@ -1036,3 +1036,31 @@ const solution47_1 = (arr, n) =>
 // 2. 앞에서부터 5개만 리턴한다. (slice())
 
 const solution48 = (num_list) => num_list.sort((a, b) => a - b).slice(0, 5);
+
+////////////// 49. 빈 배열에 추가, 삭제하기 //////////////
+
+// 문제
+// 아무 원소도 들어있지 않은 빈 배열 X가 있습니다.
+// 길이가 같은 정수 배열 arr과 boolean 배열 flag가 매개변수로 주어질 때,
+// flag를 차례대로 순회하며 flag[i]가 true라면 X의 뒤에 arr[i]를 arr[i] × 2 번 추가하고,
+/// flag[i]가 false라면 X에서 마지막 arr[i]개의 원소를 제거한 뒤 X를 return 하는 solution 함수를 작성해 주세요.
+
+// 생각
+// 1. flag 배열을 순회한다.
+// 2. 요소마다 true / false 를 판별하여 그에 맞는 연산을 진행한다.
+
+const solution49 = (arr, flag) => {
+  let X = [];
+  for (i = 0; i < flag.length; i++) {
+    if (flag[i]) {
+      for (j = 0; j < arr[i] * 2; j++) {
+        X.push(arr[i]);
+      }
+    } else {
+      for (j = 0; j < arr[i]; j++) {
+        X.pop();
+      }
+    }
+  }
+  return X;
+};
