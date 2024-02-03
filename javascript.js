20838d489dd232a00f362131b98878e2fe9ea1a9
Jjunjiii’ <drum1701@gmail.com>
2024-02-03 19:58:42 +0900

    question 58

--- javascript.js
+++ javascript.js
@@ -1236,3 +1236,17 @@ const solution56_1 = (arr, dels) => arr.filter((el) => !dels.includes(el));
 // 2. 더해준 값을 문자형으로 바꾼다.
 
 const solution57 = (a, b) => `${BigInt(a) + BigInt(b)}`;
+
+////////////// 58. 부분 문자열 //////////////
+
+// 문제
+// 어떤 문자열 A가 다른 문자열 B안에 속하면 A를 B의 부분 문자열이라고 합니다.
+//   예를 들어 문자열 "abc"는 문자열 "aabcc"의 부분 문자열입니다.
+// 문자열 str1과 str2가 주어질 때, str1이 str2의 부분 문자열이라면 1을 부분 문자열이 아니라면 0을 return하도록
+//  solution 함수를 완성해주세요.
+
+// 생각
+// 1. 포함여부를 true false 로 반환하는 includes 를 사용한다.
+// 2. 숫자형으로 바꾸어 1 또는 0 을 리턴하게 만든다.
+
+const solution58 = (str1, str2) => +str2.includes(str1);
