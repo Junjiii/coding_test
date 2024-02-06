29ab296996dd09ab0348978d8f49f6226a815de5
Jjunjiii’ <drum1701@gmail.com>
2024-02-07 00:29:27 +0900

    question 67 modified2

--- javascript.js
+++ javascript.js
@@ -1498,3 +1498,8 @@ const solution66_2 = (n, t) => {
 // 정수 n이 매개변수로 주어질 때, n이 제곱수라면 1을 아니라면 2를 return하도록 solution 함수를 완성해주세요.
 
 const solution67 = (n) => (Math.sqrt(n) % 1 === 0 ? 1 : 2);
+
+/////// 다른 사람 풀이
+const solution67_1 = (n) => {
+  return Number.isInteger(Math.sqrt(n)) ? 1 : 2;
+};
