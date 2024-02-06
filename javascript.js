e49f83dc55b956f024be41e2ae7483b279c37fae
Jjunjiii’ <drum1701@gmail.com>
2024-02-07 00:06:32 +0900

    question 66
    question 66 modified
    
    question 67 modified
    
    Revert "question 66 modified"
    
    This reverts commit 63d88c093ea100b45e6920b6ba7371b43087c6c9.
    
    question 67
    
    question 67 modified
    
    question 67 modified2
    
    question 67 modified3

--- javascript.js
+++ javascript.js
@@ -1503,3 +1503,12 @@ const solution67 = (n) => (Math.sqrt(n) % 1 === 0 ? 1 : 2);
 const solution67_1 = (n) => {
   return Number.isInteger(Math.sqrt(n)) ? 1 : 2;
 };
+
+const solution67_2 = (n) => {
+  for (let i = 0; i < n / 2; i++) {
+    if (i * i == n) {
+      return 1;
+    }
+  }
+  return 2;
+};
