803099e2f2c65dc31ecc46337e8d854b6133605b
Jjunjiii’ <drum1701@gmail.com>
2024-02-08 21:39:47 +0900

    question 68

--- javascript.js
+++ javascript.js
@@ -1512,3 +1512,11 @@ const solution67_2 = (n) => {
   }
   return 2;
 };
+
+////////////// 68. 문자열 안에 문자열 /////////////
+
+// 문제
+// 문자열 str1, str2가 매개변수로 주어집니다.
+//  str1 안에 str2가 있다면 1을 없다면 2를 return하도록 solution 함수를 완성해주세요.
+
+const solution68 = (str1, str2) => (str1.includes(str2) ? 1 : 2);
