18e941137cd6ae2b23f175bd53802722606ee4b5
Jjunjiii’ <drum1701@gmail.com>
2024-02-16 21:48:18 +0900

    question 85

--- js.lv0.js
+++ js.lv0.js
@@ -1821,3 +1821,28 @@ const solution84_2 = (my_string) => {
     arr.unshift(+arr.shift() + (arr.shift() === "+" ? 1 : -1) * arr.shift());
   return arr[0];
 };
+
+/////////////// 85. 영어가 싫어요 /////////////
+
+// 문제
+// 영어가 싫은 머쓱이는 영어로 표기되어있는 숫자를 수로 바꾸려고 합니다. 문자열 numbers가 매개변수로 주어질 때,
+// numbers를 정수로 바꿔 return 하도록 solution 함수를 완성해 주세요.
+
+const solution85 = (numbers) => {
+  let num = [
+    "zero",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+  ];
+  for (i = 0; i < num.length; i++) {
+    numbers = numbers.split(num[i]).join(i);
+  }
+  return Number(numbers);
+};
