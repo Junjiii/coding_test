7e1f8abeaaaf7a5120bcde68c288abd40767497b
Jjunjiii’ <drum1701@gmail.com>
2024-02-16 21:52:27 +0900

    question 86

--- js.lv0.js
+++ js.lv0.js
@@ -1888,3 +1888,23 @@ const solution85_2 = (n) =>
       "nine",
     ].reduce((t, s, i) => t.replaceAll(s, i), n)
   );
+
+/////////////// 86. 가까운 수 /////////////
+
+// 문제
+// 정수 배열 array와 정수 n이 매개변수로 주어질 때,
+// array에 들어있는 정수 중 n과 가장 가까운 수를 return 하도록 solution 함수를 완성해주세요.
+
+const solution86 = (array, n) => {
+  array.sort((a, b) => a - b);
+  let a = 0;
+  let b = 0;
+  let answer = [];
+
+  for (let i = 0; i < array.length; i++) {
+    answer.push(Math.abs(n - array[i]));
+    a = Math.min(...answer);
+    b = answer.indexOf(a);
+  }
+  return array[b];
+};
