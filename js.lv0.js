556b6cc90b31590cb01f3aedf3286d86c7594284
Jjunjiii’ <drum1701@gmail.com>
2024-02-19 17:25:36 +0900

    question 90

--- js.lv0.js
+++ js.lv0.js
@@ -1968,3 +1968,20 @@ const solution89 = (i, j, k) => {
   }
   return a.split(k).length - 1;
 };
+
+/////////////// 90. A로 B 만들기 /////////////
+
+// 문제
+// 문자열 before와 after가 매개변수로 주어질 때,
+// before의 순서를 바꾸어 after를 만들 수 있으면 1을, 만들 수 없으면 0을 return 하도록 solution 함수를 완성해보세요.
+
+const solution90 = (before, after) => {
+  const afterArr = [...after];
+  const arr = [...before].reduce((acc, v, i) => {
+    if (afterArr.includes(v)) {
+      afterArr.splice(afterArr.indexOf(v), 1);
+    }
+  }, "");
+
+  return afterArr.length ? 0 : 1;
+};
