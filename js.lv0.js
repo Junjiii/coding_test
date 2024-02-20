880d35bb2b4ac251182edb0e75d1d65e34765b9e
Jjunjiii’ <drum1701@gmail.com>
2024-02-20 16:56:53 +0900

    question 91

--- js.lv0.js
+++ js.lv0.js
@@ -1993,3 +1993,14 @@ const solution90_1 = (before, after) => {
     ? 1
     : 0;
 };
+
+/////////////// 91. 이진수 더하기 /////////////
+
+// 문제
+// 이진수를 의미하는 두 개의 문자열 bin1과 bin2가 매개변수로 주어질 때,
+// 두 이진수의 합을 return하도록 solution 함수를 완성해주세요.
+
+const solution91 = (bin1, bin2) => {
+  const num1 = parseInt(bin1, 2) + parseInt(bin2, 2);
+  return num1.toString(2);
+};
