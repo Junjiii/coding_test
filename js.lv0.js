9eec24b1ed12d0c0524fec04a568e593bf5f0bcf
Jjunjiii’ <drum1701@gmail.com>
2024-02-26 21:18:25 +0900

    question 104

--- js.lv0.js
+++ js.lv0.js
@@ -2272,3 +2272,31 @@ const solution103 = (n) => {
   }
   return answer;
 };
+
+/////////////// 104. 합성수 찾기 /////////////
+
+// 문제
+// 약수의 개수가 세 개 이상인 수를 합성수라고 합니다.
+// 자연수 n이 매개변수로 주어질 때 n이하의 합성수의 개수를 return하도록 solution 함수를 완성해주세요.
+
+const solution104 = (n) => {
+  let ans = [];
+
+  for (let i = 1; i <= n; i++) {
+    let measure = [];
+
+    for (let j = 1; j <= n; j++) {
+      if (i % j === 0) {
+        measure.push(j);
+      }
+
+      if (measure.length >= 3) {
+        ans.push(i);
+
+        break;
+      }
+    }
+  }
+
+  return ans.length;
+};
