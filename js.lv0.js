45de7d3c300d68b38cbae94a658231394bc77d88
Jjunjiii’ <drum1701@gmail.com>
2024-03-01 21:40:48 +0900

    question 108

--- js.lv0.js
+++ js.lv0.js
@@ -2421,3 +2421,24 @@ const solution107_1 = (dot) => {
   const check = num * num2 > 0;
   return num > 0 ? (check ? 1 : 4) : check ? 3 : 2;
 };
+
+/////////////// 108. 가위 바위 보 /////////////
+
+// 문제
+// 가위는 2 바위는 0 보는 5로 표현합니다. 가위 바위 보를 내는 순서대로 나타낸 문자열 rsp가 매개변수로 주어질 때,
+// rsp에 저장된 가위 바위 보를 모두 이기는 경우를 순서대로 나타낸 문자열을 return하도록 solution 함수를 완성해보세요.
+
+const solution108 = (rsp) => {
+  return [...rsp].map((v) => (v == 2 ? 0 : v == 0 ? 5 : 2)).join("");
+};
+
+/////// 다른 사람 풀이
+const solution108_1 = (rsp) => {
+  let arr = {
+    2: 0,
+    0: 5,
+    5: 2,
+  };
+  var answer = [...rsp].map((v) => arr[v]).join("");
+  return answer;
+};
