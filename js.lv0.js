a5a47f3ac521521c56c46d4796c2687b0dd28c59
Jjunjiii’ <drum1701@gmail.com>
2024-03-02 19:37:43 +0900

    question 111 1

--- js.lv0.js
+++ js.lv0.js
@@ -2526,3 +2526,12 @@ const solution111 = (n) => {
   }
   return arr.length;
 };
+
+/////// 다른 사람 풀이
+
+const solution111_1 = (n) => {
+  let ans = 0;
+  for (let i = 1; i < Math.sqrt(n); i++) if (n % i === 0) ans += 2;
+
+  return Number.isInteger(Math.sqrt(n)) ? ans + 1 : ans;
+};
