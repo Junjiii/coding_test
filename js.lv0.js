d7261640f96dfb06897e98361cad192b82174892
Jjunjiii’ <drum1701@gmail.com>
2024-03-02 19:36:49 +0900

    question 111

--- js.lv0.js
+++ js.lv0.js
@@ -2507,3 +2507,22 @@ const solution110 = (hp) => {
 const solution110_1 = (hp) => {
   return Math.floor(hp / 5) + Math.floor((hp % 5) / 3) + ((hp % 5) % 3);
 };
+
+/////////////// 111. 순서쌍의 개수  /////////////
+
+// 문제
+// 순서쌍이란 두 개의 숫자를 순서를 정하여 짝지어 나타낸 쌍으로 (a, b)로 표기합니다.
+// 자연수 n이 매개변수로 주어질 때 두 숫자의 곱이 n인 자연수 순서쌍의 개수를 return하도록 solution 함수를 완성해주세요.
+
+const solution111 = (n) => {
+  const arr = [];
+  let i = 1;
+
+  while (i <= n) {
+    if (n % i === 0) {
+      arr.push([i, n / i]);
+    }
+    i++;
+  }
+  return arr.length;
+};
