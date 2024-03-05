21b57fdfeb60febac042f54751412dd9bac901f7
Jjunjiii’ <drum1701@gmail.com>
2024-03-05 22:40:30 +0900

    question 115

--- js.lv0.js
+++ js.lv0.js
@@ -2607,3 +2607,16 @@ const solution113_3 = (age) => {
 const solution114 = (numbers, num1, num2) => {
   return numbers.slice(num1, num2 + 1);
 };
+
+/////////////// 115. 짝수의 합  /////////////
+
+// 문제
+// 정수 n이 주어질 때, n이하의 짝수를 모두 더한 값을 return 하도록 solution 함수를 작성해주세요.
+
+const solution115 = (n) => {
+  let result = 0;
+  for (i = 0; i <= n; i += 2) {
+    result += i;
+  }
+  return result;
+};
