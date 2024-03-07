0b398265734f16f010fe7b20137687ecfd5f1035
Jjunjiii’ <drum1701@gmail.com>
2024-03-07 09:02:57 +0900

    question 118

--- js.lv0.js
+++ js.lv0.js
@@ -2671,3 +2671,13 @@ const solution117_3 = (angle) => {
       return angle > 0 && angle < 90 ? 1 : 3;
   }
 };
+
+/////////////// 118. 특정 문자 제거하기  /////////////
+
+// 문제
+// 문자열 my_string과 문자 letter이 매개변수로 주어집니다.
+// my_string에서 letter를 제거한 문자열을 return하도록 solution 함수를 완성해주세요.
+
+const solution118 = (my_string, letter) => {
+  return [...my_string].filter((v) => v !== letter).join("");
+};
