152450b05968f98c8341c3e0308dd048c75dd5e4
Jjunjiii’ <drum1701@gmail.com>
2024-03-09 15:01:58 +0900

    question 120 2

--- js.lv0.js
+++ js.lv0.js
@@ -2737,3 +2737,10 @@ const solution120_1 = (num_list) => {
 
   return answer;
 };
+
+const solution120_2 = (num_list) => {
+  return [
+    num_list.filter((num) => num % 2 === 0).length,
+    num_list.filter((num) => num % 2 === 1).length,
+  ];
+};
