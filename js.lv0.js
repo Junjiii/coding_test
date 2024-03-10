bae62e307571b6f7935519f97e1fc17779e22900
Jjunjiii’ <drum1701@gmail.com>
2024-03-10 21:19:16 +0900

    question 121

--- js.lv0.js
+++ js.lv0.js
@@ -2751,3 +2751,11 @@ const solution120_3 = (list) => {
     [0, 0]
   );
 };
+
+/////////////// 121. 문자열 뒤집기  /////////////
+
+// 문제
+// 문자열 my_string이 매개변수로 주어집니다.
+// my_string을 거꾸로 뒤집은 문자열을 return하도록 solution 함수를 완성해주세요.
+
+const sotution121 = (my_string) => [...my_string].reverse().join("");
