652d8a58fa5e488fc39cd3ac20c6491738055d18
Jjunjiii’ <drum1701@gmail.com>
2024-03-12 15:51:57 +0900

    question 124

--- js.lv0.js
+++ js.lv0.js
@@ -2775,3 +2775,23 @@ const solution122 = (num_list) => num_list.reverse();
 // 나이 age가 주어질 때, 2022년을 기준 출생 연도를 return 하는 solution 함수를 완성해주세요.
 
 const solution = (age) => 2022 - (age - 1);
+
+/////////////// 124. 아이스 아메리카노  /////////////
+
+// 문제
+// 머쓱이는 추운 날에도 아이스 아메리카노만 마십니다.
+// 아이스 아메리카노는 한잔에 5,500원입니다. 머쓱이가 가지고 있는 돈 money가 매개변수로 주어질 때,
+//  머쓱이가 최대로 마실 수 있는 아메리카노의 잔 수와 남는 돈을 순서대로 담은 배열을 return 하도록 solution 함수를 완성해보세요.
+
+const solution124 = (money) => {
+  let result = [0, money];
+  while (result[1] > 0) {
+    if (result[1] >= 5500) {
+      result[1] -= 5500;
+      result[0] += 1;
+    } else {
+      break;
+    }
+  }
+  return result;
+};
