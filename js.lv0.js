deaf5edfa19a5ee46c0f65e39ad190e6410fbd35
Jjunjiii’ <drum1701@gmail.com>
2024-03-14 14:10:30 +0900

    question 125

--- js.lv0.js
+++ js.lv0.js
@@ -2810,3 +2810,21 @@ const solution124_1 = (money) => {
 const solution124_2 = (money) => {
   return [Math.floor(money / 5500), money % 5500];
 };
+
+/////////////// 125. 옷가게 할인 받기  /////////////
+
+// 문제
+// 머쓱이네 옷가게는 10만 원 이상 사면 5%, 30만 원 이상 사면 10%, 50만 원 이상 사면 20%를 할인해줍니다.
+// 구매한 옷의 가격 price가 주어질 때, 지불해야 할 금액을 return 하도록 solution 함수를 완성해보세요.
+
+const solution125 = (price) => {
+  if (price >= 100000 && price < 300000) {
+    return Math.floor(price * 0.95);
+  } else if (price >= 300000 && price < 500000) {
+    return Math.floor(price * 0.9);
+  } else if (price >= 500000) {
+    return Math.floor(price * 0.8);
+  } else {
+    return price;
+  }
+};
