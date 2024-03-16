6ea3a2fa6d97b977dc4ca1a436c6c6daf6ce5d51
Jjunjiii’ <drum1701@gmail.com>
2024-03-16 23:16:03 +0900

    question 126

--- js.lv0.js
+++ js.lv0.js
@@ -2828,3 +2828,28 @@ const solution125 = (price) => {
     return price;
   }
 };
+
+/////////////// 126. 공 던지기  /////////////
+
+// 문제
+// 머쓱이는 친구들과 동그랗게 서서 공 던지기 게임을 하고 있습니다.
+// 공은 1번부터 던지며 오른쪽으로 한 명을 건너뛰고 그다음 사람에게만 던질 수 있습니다.
+// 친구들의 번호가 들어있는 정수 배열 numbers와 정수 K가 주어질 때,
+// k번째로 공을 던지는 사람의 번호는 무엇인지 return 하도록 solution 함수를 완성해보세요.
+
+const solution126 = (numbers, k) => {
+  let arr = [];
+  let count = 0;
+
+  while (arr.length <= k - 1) {
+    if (count > numbers.length - 1) {
+      count = count - numbers.length;
+      arr.push(numbers[count]);
+      count += 2;
+    } else {
+      arr.push(numbers[count]);
+      count += 2;
+    }
+  }
+  return arr[arr.length - 1];
+};
