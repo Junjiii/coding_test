c78dd3e49c654e51d55b387607f0ac706541896a
Jjunjiii’ <drum1701@gmail.com>
2024-04-05 14:12:38 +0900

    question 131  1

--- js.lv0.js
+++ js.lv0.js
@@ -2993,3 +2993,26 @@ const solution131 = (keyinput, board) => {
     [0, 0]
   );
 };
+
+//////// 다른 사람 풀이
+
+const solution131_1 = (keyinput, board) => {
+  let res = [0, 0];
+  for (let p of keyinput) {
+    switch (p) {
+      case "left":
+        if (-res[0] < board[0] / 2 - 1) res[0]--;
+        break;
+      case "right":
+        if (res[0] < board[0] / 2 - 1) res[0]++;
+        break;
+      case "up":
+        if (res[1] < board[1] / 2 - 1) res[1]++;
+        break;
+      case "down":
+        if (-res[1] < board[1] / 2 - 1) res[1]--;
+        break;
+    }
+  }
+  return res;
+};
