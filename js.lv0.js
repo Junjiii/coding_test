269fac2ec89c96305f63ef59bbb93cfddade2cfb
Jjunjiii’ <drum1701@gmail.com>
2024-04-06 13:46:45 +0900

    question 132

--- js.lv0.js
+++ js.lv0.js
@@ -3016,3 +3016,44 @@ const solution131_1 = (keyinput, board) => {
   }
   return res;
 };
+
+/////////////// 132. 다항식 더하기   /////////////
+
+// 문제
+// 한 개 이상의 항의 합으로 이루어진 식을 다항식이라고 합니다.
+// 다항식을 계산할 때는 동류항끼리 계산해 정리합니다.
+// 덧셈으로 이루어진 다항식 polynomial이 매개변수로 주어질 때,
+// 동류항끼리 더한 결괏값을 문자열로 return 하도록 solution 함수를 완성해보세요.
+// 같은 식이라면 가장 짧은 수식을 return 합니다.
+
+const solution132 = (polynomial) => {
+  let arr = polynomial.split(" + ");
+  let xTerm = 0;
+  let constant = 0;
+  arr.map((i) => {
+    if (i.includes("x")) {
+      const tmp = i.split("x");
+      tmp[0] ? (xTerm += parseInt(tmp[0])) : xTerm++;
+    } else if (i !== "+") {
+      constant += parseInt(i);
+    }
+  });
+  if (xTerm !== 0 && constant !== 0) {
+    if (xTerm === 1) {
+      return `x + ${constant}`;
+    }
+    return `${xTerm}x + ${constant}`;
+  }
+  if (xTerm === 0 && constant !== 0) {
+    return `${constant}`;
+  }
+  if (xTerm !== 0 && constant === 0) {
+    if (xTerm === 1) {
+      return "x";
+    }
+    return `${xTerm}x`;
+  }
+  if (xTerm === 0 && constant === 0) {
+    return "0";
+  }
+};
