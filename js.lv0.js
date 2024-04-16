318d6fc08ed530729fbcf9440a8bedee8c3d334d
Jjunjiii’ <drum1701@gmail.com>
2024-04-16 23:58:16 +0900

    question 137 1

--- js.lv0.js
+++ js.lv0.js
@@ -3246,3 +3246,19 @@ const solution137 = (arr) => {
   }
   return str;
 };
+
+///////////// 다른 사람 풀이
+
+const solution137_1 = (arr) => {
+  var stk = [];
+  for (let i = 0; i < arr.length; ) {
+    if (stk.length === 0) {
+      stk.push(arr[i++]);
+    } else if (stk[stk.length - 1] < arr[i]) {
+      stk.push(arr[i++]);
+    } else if (stk[stk.length - 1] >= arr[i]) {
+      stk.pop();
+    }
+  }
+  return stk;
+};
