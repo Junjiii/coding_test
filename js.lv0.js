fd8fc564df702d5a9253fc44da3570bd29a8324f
Jjunjiii’ <drum1701@gmail.com>
2024-04-18 23:32:17 +0900

    question 139

--- js.lv0.js
+++ js.lv0.js
@@ -3289,3 +3289,21 @@ const solution138_1 = (p, d) => {
 const solution138_2 = (spell, dic) => {
   return dic.filter((v) => spell.every((c) => v.includes(c))).length ? 1 : 2;
 };
+
+////////////// 139. 문자열 여러번 뒤집기 /////////////
+
+// 문제
+// 문자열 my_string과 이차원 정수 배열 queries가 매개변수로 주어집니다.
+//  queries의 원소는 [s, e] 형태로, my_string의 인덱스 s부터 인덱스 e까지를 뒤집으라는 의미입니다.
+// my_string에 queries의 명령을 순서대로 처리한 후의 문자열을 return 하는 solution 함수를 작성해 주세요.
+
+const solution139 = (my_string, queries) => {
+  for (let [s, e] of queries) {
+    let word = my_string.slice(s, e + 1);
+    my_string =
+      my_string.slice(0, s) +
+      word.split("").reverse().join("") +
+      my_string.slice(e + 1);
+  }
+  return my_string;
+};
