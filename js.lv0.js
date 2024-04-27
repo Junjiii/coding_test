0932d16cdbbfdeaa929b283eac97cc9123433d9c
Jjunjiii’ <drum1701@gmail.com>
2024-04-27 13:26:36 +0900

    question 143

--- js.lv0.js
+++ js.lv0.js
@@ -3414,3 +3414,25 @@ function solution142_1(q, r, code) {
     return result;
   }, "");
 }
+
+////////////// 143. 문자 개수 세기 /////////////
+
+// 문제
+// 알파벳 대소문자로만 이루어진 문자열 my_string이 주어질 때,
+// my_string에서 'A'의 개수, my_string에서 'B'의 개수,..., my_string에서 'Z'의 개수,
+// my_string에서 'a'의 개수, my_string에서 'b'의 개수,..., my_string에서 'z'의 개수를
+// 순서대로 담은 길이 52의 정수 배열을 return 하는 solution 함수를 작성해 주세요.
+
+function solution143(m) {
+  var answer = [];
+  let al = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  let a = [];
+  a.length = 52;
+  a.fill(0);
+
+  m.split("").map((n) => {
+    a[al.indexOf(n)] += 1;
+  });
+
+  return a;
+}
