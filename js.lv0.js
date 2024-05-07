f65e66779f6acc895d445d76080c42279b2ac976
Jjunjiii’ <drum1701@gmail.com>
2024-05-07 09:25:35 +0900

    question 152 1

--- js.lv0.js
+++ js.lv0.js
@@ -3769,3 +3769,16 @@ function solution152(A, B) {
 
   return result;
 }
+
+///////// 다른 사람 풀이
+
+let solution152_1 = (a, b) => (b + b).indexOf(a);
+
+function solution152_2(A, B) {
+  if (A === B) return 0;
+  for (let i = 0; i < A.length; i++) {
+    A = A.slice(-1) + A.slice(0, -1);
+    if (A === B) return i + 1;
+  }
+  return -1;
+}
